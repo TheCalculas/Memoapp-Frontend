3c5fd6ade3fab2373921d3bd89761b1d36db6045
dharmesh <onlinedk2k@gmail.com>
2023-08-01 13:22:40 +0530

    commit1

--- src/Context/Notes/NotesState.jsx
+++ src/Context/Notes/NotesState.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import NoteContext from "./NoteContext";
 
 const NotesState = (props) => {
-  const host = "https://memoapp-backend.vercel.app/";
+  const host = "https://memoapp-backend.vercel.app";
   const notesInit = [];
   const [notes, setNotes] = useState(notesInit);
 
